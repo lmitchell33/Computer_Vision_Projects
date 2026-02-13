aef9f7ce72047180774c0971a986b9a3e0140b75
Lucas Mitchell <lhmitchell1333@gmail.com>
2026-02-13 02:35:35 -0500

    created the new "experiments" and modularized to run separately

--- Project1/main.py
+++ Project1/main.py
@@ -42,11 +42,14 @@ def temporal_derivative(frames, kernel):
     
     return derivatives
 
-def threshold_frames(frames, threshold=None):
+def threshold_frames(frames, threshold=None, n_val=None):
     """Thresholds the frames and creates a mask of 0 or 1s. #3 in teh breakdown of tasks"""
     # should create "bitmasks" of 0s and 1s based on threshold
     if threshold:
         return [(np.abs(frame) > threshold).astype(np.uint8) for frame in frames]
+
+    if n_val:
+        return [(np.abs(frame) > find_good_threshold(frame, n_val)).astype(np.uint8) for frame in frames]
 
     return [(np.abs(frame) > find_good_threshold(frame)).astype(np.uint8) for frame in frames]
 
@@ -57,7 +60,6 @@ def apply_masks(frames, masks):
         result = np.copy(frame) # you have add this line or else it will apply the mask to the original images stored in frames
         # result = result * mask
         result[mask == 0] = 0
-        # result[mask == 1] = 255 
         output.append(result)
 
     return output
@@ -84,6 +86,27 @@ def derivative_gaussian(frames, tsigma, threshold=None):
     masks = threshold_frames(result, threshold)
     motion_frames = apply_masks(frames, masks)  # Pass original list
     return motion_frames
+
+def dog_experiment(frames, temporal_kernel, t_sigmas):
+    # just two, pure temporal and a dog
+    d_frames = temporal_derivative(frames, temporal_kernel)
+    masks = threshold_frames(d_frames)
+    pure_temporal_frames = apply_masks(frames, masks)
+    title = "original | pure temporal filter | "
+
+    # this should be formatted as a list of three lists which contain the frames from each indiviual t_sigma
+    dog_outputs = [derivative_gaussian(frames=frames,tsigma=t_sigma) for t_sigma in t_sigmas]
+    title += "| ".join([f"t_sigma={t_sigma}" for t_sigma in t_sigmas])
+
+    for index in range(len(frames)):
+        frame = frames[index]
+        combined = np.hstack([frame, pure_temporal_frames[index]] + [dog_outputs[j][index] for j in range(len(dog_outputs))])
+        cv2.imshow(title, combined)
+        key = cv2.waitKey(30)
+        if key == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
 
 
 
@@ -117,52 +140,114 @@ def smooth_then_temporal(frames, temporal_kernel, threshold=None, s_sigma=0, use
     return motion_frames
 
 
-
-def find_good_threshold(frame):
-    """#3 in the different variations"""
-    # 1 std away is little low and keeps way too much noise but 2 and 3 both seem decent but 3 is the best in my opionin  
-    return 3*np.std(frame)
-
-
-def main():    
-    # frames = read_frames(OFFICE_IMAGES)
-    # frames = read_frames(REDCHAIR_IMAGES)
-    frames = read_frames(ENTEREXIT_IMAGES)
-
-    temporal_kernel = 0.5 * np.array([-1, 0, 1])
-    s_sigmas = [0.30, 2, 10] # idk
-    t_sigmas = [0.30, 2, 10]
-    thresholds = [5, 25, 50] # idk
-    sizes = [(3,3), (5,5)]
-
-    # right now they are all just using the calculated thresholds
+def guass_experiment(frames, s_sigmas, sizes, temporal_kernel):
+    
+    title = "Original |"
     box_outputs =  [smooth_then_temporal(frames=frames, temporal_kernel=temporal_kernel, use_box=True, size=size) for size in sizes]
+    title += "| ".join([f"size = {size}" for size in sizes])
+
     guass_outputs = [smooth_then_temporal(frames=frames, temporal_kernel=temporal_kernel, s_sigma=s_sigma, use_box=False, size=(3,3)) for s_sigma in s_sigmas]
-    dog_outputs = [derivative_gaussian(frames=frames,tsigma=t_sigma) for t_sigma in t_sigmas]
+    title += "| ".join([f"s_sigma = {s_sigma}" for s_sigma in s_sigmas])
 
     for index in range(len(frames)):
         frame = frames[index]
-        dog_0 = dog_outputs[0][index]
-        dog_1 = dog_outputs[1][index]
-        dog_2 = dog_outputs[2][index]
-        dog_filter_frames = np.hstack((dog_0, dog_1, dog_2))
-
-        box_output_1 = box_outputs[0][index]
-        box_output_2 = box_outputs[1][index]
-        box_filter_frames = np.hstack((frame, box_output_1, box_output_2))
-
-        gauss_0 = guass_outputs[0][index]
-        gauss_1 = guass_outputs[1][index]
-        gauss_2 = guass_outputs[2][index]
-        gauss_filter_frames = np.hstack((gauss_0, gauss_1, gauss_2))
-
-        combined = np.vstack((dog_filter_frames, box_filter_frames, gauss_filter_frames))
-        cv2.imshow(f"t_sigma={t_sigmas[0]}, t_sigma={t_sigmas[1]}, t_sigma={t_sigmas[2]} | Original, box_size={sizes[0]}, box_size={sizes[1]} | s_sigma={s_sigmas[0]}, s_sigma={s_sigmas[1]}, s_sigma={s_sigmas[2]}", combined)
+        boxes = np.hstack([frame] + [box_outputs[j][index] for j in range(len(box_outputs))])
+        guasses = np.hstack([guass_outputs[j][index] for j in range(len(guass_outputs))])
+        combined = np.vstack((boxes, guasses))    
+        cv2.imshow(title, combined)
         key = cv2.waitKey(30)
         if key == ord("q"):
             break
 
     cv2.destroyAllWindows()
+
+
+
+def find_good_threshold(frame, n=3):
+    """#3 in the different variations"""
+    # 1 std away is little low and keeps way too much noise but 2 and 3 both seem decent but 3 is the best in my opionin  
+    return n*np.std(frame)
+
+def thresholds_experiment(frames, fixed_thresholds, n_vals, temporal_kernel):
+    smoothed_frames = box_filter(frames, (3,3))
+    d_frames = temporal_derivative(smoothed_frames, temporal_kernel)
+    
+    # dont think that I need to worry about the copying stuff here because all the other functions deal with that
+    fixed_threshold_frames = []
+    title = "Original | "
+    for threshold in fixed_thresholds:
+        masks = threshold_frames(d_frames, threshold=threshold)
+        motion_frames = apply_masks(frames, masks)
+        fixed_threshold_frames.append(motion_frames)
+        title += f"fixed threshold = {threshold} | "
+
+    n_val_frames = []
+    for n in n_vals:
+        masks = threshold_frames(d_frames, n_val=n)
+        motion_frames = apply_masks(frames, masks)
+        n_val_frames.append(motion_frames)
+        title += f"n = {n} | "
+
+    for index in range(len(frames)):
+        frame = frames[index]
+
+        # its being weird because of the non-symmetrical h stacks, so I have to add the original frame ot each of the horizontal stacks 
+        curr_fixed_threshold_frames = np.hstack(([frame] + [fixed_threshold_frames[j][index] for j in range(len(fixed_thresholds))]))
+        curr_n_threshold_frames = np.hstack(([frame] + [n_val_frames[j][index] for j in range(len(n_vals))]))
+
+        combined = np.vstack((curr_fixed_threshold_frames, curr_n_threshold_frames))
+        cv2.imshow(title, combined)
+        key = cv2.waitKey(30)
+        if key == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+
+
+def main():
+    # frames = read_frames(OFFICE_IMAGES)
+    # frames = read_frames(REDCHAIR_IMAGES)
+    frames = read_frames(ENTEREXIT_IMAGES)
+    temporal_kernel = 0.5 * np.array([-1, 0, 1])
+    s_sigmas = [0.10, 1, 10] 
+    sizes = [(3,3), (5,5)]
+    t_sigmas = [0.10, 1, 10]
+    thresholds = [5, 25, 50] 
+    n_vals = [1, 2, 3]
+    guass_experiment(frames, s_sigmas, sizes, temporal_kernel)
+    dog_experiment(frames, temporal_kernel, t_sigmas)
+    thresholds_experiment(frames, thresholds, n_vals, temporal_kernel)
+
+
+    # old code 
+    # right now they are all just using the calculated thresholds
+    # box_outputs =  [smooth_then_temporal(frames=frames, temporal_kernel=temporal_kernel, use_box=True, size=size) for size in sizes]
+    # guass_outputs = [smooth_then_temporal(frames=frames, temporal_kernel=temporal_kernel, s_sigma=s_sigma, use_box=False, size=(3,3)) for s_sigma in s_sigmas]
+    # dog_outputs = [derivative_gaussian(frames=frames,tsigma=t_sigma) for t_sigma in t_sigmas]
+
+    # for index in range(len(frames)):
+    #     frame = frames[index]
+    #     dog_0 = dog_outputs[0][index]
+    #     dog_1 = dog_outputs[1][index]
+    #     dog_2 = dog_outputs[2][index]
+    #     dog_filter_frames = np.hstack((dog_0, dog_1, dog_2))
+
+    #     box_output_1 = box_outputs[0][index]
+    #     box_output_2 = box_outputs[1][index]
+    #     box_filter_frames = np.hstack((frame, box_output_1, box_output_2))
+
+    #     gauss_0 = guass_outputs[0][index]
+    #     gauss_1 = guass_outputs[1][index]
+    #     gauss_2 = guass_outputs[2][index]
+    #     gauss_filter_frames = np.hstack((gauss_0, gauss_1, gauss_2))
+
+    #     combined = np.vstack((dog_filter_frames, box_filter_frames, gauss_filter_frames))
+    #     cv2.imshow(f"t_sigma={t_sigmas[0]}, t_sigma={t_sigmas[1]}, t_sigma={t_sigmas[2]} | Original, box_size={sizes[0]}, box_size={sizes[1]} | s_sigma={s_sigmas[0]}, s_sigma={s_sigmas[1]}, s_sigma={s_sigmas[2]}", combined)
+    #     key = cv2.waitKey(30)
+    #     if key == ord("q"):
+    #         break
+
+    # cv2.destroyAllWindows()
     
 if __name__ == "__main__":
     main()
